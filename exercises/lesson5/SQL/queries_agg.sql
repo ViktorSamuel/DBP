3baf07b07f9b2e811d471dd3394ed1bcfc1b6b10
Viktor Samuel Podhradský <samkopodhradsky@gmail.com>
2023-11-08 08:24:55 +0100

    lesson 5

--- exercises/lesson5/SQL/queries_agg.sql
+++ exercises/lesson5/SQL/queries_agg.sql
@@ -7,7 +7,6 @@ emp(empno, name, job, superior, hiredate, salary, deptno)
 */
 
 /* 1. Find the number of employees who work in Dallas. */
-
 SELECT COUNT(DISTINCT e.empno)
 FROM emp e, dept d
 WHERE e.deptno = d.deptno and d.location = 'Dallas';
@@ -25,20 +24,34 @@ SELECT deptno FROM emp GROUP BY deptno HAVING count(*) > 3;
 SELECT d.name, count(e.empno) FROM emp e JOIN dept d ON e.deptno = d.deptno WHERE e.job = 'analyst' GROUP BY d.deptno;
 
 /* 6. Find the jobs with the maximal standard deviation of salaries. */
--- WITH SalaryStdDevs AS (
---     SELECT job, STDDEV(salary) AS salary_stddev
---     FROM emp
---     GROUP BY job
--- )
--- SELECT job, salary_stddev
--- FROM SalaryStdDevs
--- WHERE salary_stddev = (SELECT MAX(salary_stddev) FROM SalaryStdDevs);
-
+WITH SalaryStdDevs AS (
+    SELECT job, STDDEV(salary) AS salary_stddev
+    FROM emp
+    GROUP BY job
+)
+SELECT job, salary_stddev
+FROM SalaryStdDevs
+WHERE salary_stddev = (SELECT MAX(salary_stddev) FROM SalaryStdDevs);
 
 /* 7. Find tuples [D, J, Sum, Average] which, for each pair [D, J], state the sum of salaries and average salary of employees who work in department D and do job J. */
+SELECT deptno, job, sum(salary), avg(salary) FROM emp GROUP BY deptno, job;
 
 /* 8. Find tuples [Y, N], where N is number of employees hired in the year Y (the resuslt contains only years when an employee was hired) */
+SELECT EXTRACT(YEAR FROM hiredate) AS year, COUNT(*) AS num FROM emp GROUP BY year ORDER BY year;  
 
 /* 9. For each year since 1990, find number of employees hired in that year (the resuslt must contain also years when no employee was hired). Hint: use PostgreSQL function generate_series(). */
+SELECT EXTRACT(YEAR FROM hiredate) AS year, COUNT(*) AS num FROM emp WHERE year > 1900 GROUP BY year ORDER BY year;  
 
 /* 10. For each employee, find the number of subordinates (both direct and indirect) of that employee. Include employees with no subordinates. */
+WITH RECURSIVE Subordinates(empno, superior, level) AS (
+    SELECT empno, superior, 0
+    FROM emp
+    WHERE superior IS NULL
+    UNION ALL
+    SELECT e.empno, e.superior, s.level + 1
+    FROM emp e, Subordinates s
+    WHERE e.superior = s.empno
+)
+SELECT empno, level
+FROM Subordinates
+ORDER BY empno;
